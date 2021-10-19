6d6a2fd03a40933cfc56dd254056b2233ec97196
Chinemerem Jideama <ncjideama1@gmail.com>
2021-10-20 00:45:26 +0100

    basic installations

--- app/Http/Controllers/PagesController.php
+++ app/Http/Controllers/PagesController.php
@@ -17,6 +17,10 @@ class PagesController extends Controller
     }
 
     public function services(){
-        return view('pages.services');
+        $data = array(
+            'title' => 'Services',
+            'services' => ['Web design','Programming','SEO']
+        );
+        return view('pages.services')->with($data);
     }
 }
