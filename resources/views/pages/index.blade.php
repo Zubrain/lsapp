f8ddc7647621b816cd0133797b4aef861ca5a18f
Chinemerem Jideama <ncjideama1@gmail.com>
2021-10-20 01:44:10 +0100

    blade templating

--- resources/views/pages/index.blade.php
+++ resources/views/pages/index.blade.php
@@ -1,7 +1,11 @@
 @extends('layouts.app')
 
 @section('content')
+<div class="jumbotron text-center bg-light my-5 py-5">
     <h1>{{$title}}</h1>
     <p>This is Laravel Application from the "Laravel from Scratch" Youtube Series</p>
+    <a class="btn btn-primary btn-lg" href="/login" role="button">Login</a> <a class="btn btn-success btn-lg" href="/register" role="button">Register</a>
+</div>
+    
 @endsection
         
